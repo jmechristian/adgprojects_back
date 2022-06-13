248543c11ed9f6b94f373b06e8f65214d0cf4eb3
JMEChristian Digital <jmechristian@gmail.com>
2022-06-13 14:56:04 -0400

    spaces provider

--- config/database.js
+++ config/database.js
@@ -1,13 +1,33 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+          ssl: {
+            rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+          },
+        },
+        debug: false,
+      },
+    };
+  }
+  return {
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'adgprojects'),
-      user: env('DATABASE_USERNAME', 'jamiechristian'),
-      password: env('DATABASE_PASSWORD', 'cowboy45'),
-      ssl: env.bool('DATABASE_SSL', false),
+      client: "postgres",
+      connection: {
+        host: env("DATABASE_HOST", "127.0.0.1"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "adgprojects"),
+        user: env("DATABASE_USERNAME", "jamiechristian"),
+        password: env("DATABASE_PASSWORD", "cowboy45"),
+        ssl: env.bool("DATABASE_SSL", false),
+      },
     },
-  },
-});
+  };
+};
