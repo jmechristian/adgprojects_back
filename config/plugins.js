4a62a5ee0a1dd082755d77bf0e214aaec8676bba
JMEChristian Digital <jmechristian@gmail.com>
2022-06-13 16:02:30 -0400

    do plugin

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,13 @@
 module.exports = {
   upload: {
     config: {
-      provider: "strapi-provider-upload-dos",
+      provider: "strapi-provider-upload-do",
       providerOptions: {
         key: process.env.DO_SPACE_ACCESS_KEY,
         secret: process.env.DO_SPACE_SECRET_KEY,
         endpoint: process.env.DO_SPACE_ENDPOINT,
         space: process.env.DO_SPACE_BUCKET,
         directory: process.env.DO_SPACE_DIRECTORY,
-        cdn: process.env.DO_SPACE_CDN,
       },
     },
   },
