50c3e56c1fa13ba7bb659bebf453332dc17190da
JMEChristian Digital <jmechristian@gmail.com>
2022-07-15 12:43:20 -0400

    again

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -4,12 +4,6 @@
  *  project controller
  */
 
-const collectionType = "project";
+const { createCoreController } = require("@strapi/strapi").factories;
 
-const schema = require(`../content-types/${collectionType}/schema.json`);
-const createPopulatedController = require("../../../helpers/populate");
-
-module.exports = createPopulatedController(
-  `api::${collectionType}.${collectionType}`,
-  schema
-);
+module.exports = createCoreController("api::project.project");
