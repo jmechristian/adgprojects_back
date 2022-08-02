2d6559bfc162929ace9cfb55fbe1b38621561a5c
JMEChristian Digital <jmechristian@gmail.com>
2022-08-02 15:47:51 -0400

    populate sub

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -35,7 +35,7 @@ const uid = "api::project.project";
 const components = {
   hero: true,
   department: true,
-  subcategory: true,
+  subcategory: { populate: { subcategory: true } },
   building_type: true,
   project_types: { populate: { type: true } },
   above_quote: {
